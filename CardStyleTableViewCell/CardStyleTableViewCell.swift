1ba3ee29b2d67ab80611e4d34c60696dec13656d
Xin Hong <hongxinhope@gmail.com>
2016-01-20 11:37:44 +0800

    Initial Commit

---
+++ CardStyleTableViewCell/CardStyleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardStyleTableViewCell.swift
+//  CardStyleTableViewCell
+//
+//  Created by 洪鑫 on 16/1/20.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class CardStyleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CardStyleTableViewCellExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardStyleTableViewCellExample
+//
+//  Created by 洪鑫 on 16/1/20.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
